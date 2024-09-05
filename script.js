9e816f641b032863a79bdc3e7934a29bc6d8d941
Ubaid Ahmad <ubaidahmad2468@gmail.com>
2024-09-05 22:21:42 +0500

    #

--- script.js
+++ script.js
@@ -9,17 +9,6 @@ var  tl = gsap.timeline({
         pin : true
     },
 });
-var tl = gsap.timeline({
-    scrollTrigger : {
-        scroller : 'body',
-        trigger: '.cards h1',
-        start : 'top 50%',
-        end : 'bottom 40%',
-        scrub : 2,
-        markers: true,
-    },
-})
-
 
 tl.to('.video',{
     '--clip' : 0,
@@ -38,24 +27,19 @@ tl
     xPercent:10,
     stagger : 0.03,
 }, 'b')
-
-tl.to('.c1',{
-    width : '80%',
-    backgroundColor : '#000000',
-    color : '#AEDEE0',
-})
-.to('.c2',{
-    width : '80%',
-    backgroundColor : '#000000',
-    color : '#AEDEE0',
-})
-.to('.c3',{
-    width : '80%',
-    backgroundColor : '#000000',
-    color : '#AEDEE0',
-})
-.to('.c4',{
-    width : '80%',
-    backgroundColor : '#000000',
-    color : '#AEDEE0',
+const cards = document.querySelectorAll(".cards")
+cards.forEach(function(card){
+    gsap.to(card,{
+        scrollTrigger : {
+            scroller : 'body',
+            trigger: card,
+            start : 'top 60%',
+            end : 'top 60%',
+            scrub : 1,
+        },
+        width : '80%',
+        backgroundColor : '#000000',
+        color : '#AEDEE0',
+    })
+    
 })
